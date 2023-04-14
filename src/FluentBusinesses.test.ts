88f9400d70e57f5350d734f3ebc6d963ba28170e
Samuel Hernandez <71301115+elsamird003@users.noreply.github.com>
2023-04-13 23:12:31 -0400

    HW6

---
+++ src/FluentBusinesses.test.ts
@@ -0,0 +1,59 @@
+import assert from "assert";
+import { Business } from "../include/data.js";
+import { FluentBusinesses } from "./FluentBusinesses";
+
+const testData: Business[] = [
+  {
+    business_id: "abcd",
+    name: "Applebee's",
+    city: "Charlotte",
+    state: "NC",
+    stars: 4,
+    review_count: 6,
+  },
+  {
+    business_id: "abcd",
+    name: "China Garden",
+    state: "NC",
+    city: "Charlotte",
+    stars: 4,
+    review_count: 10,
+  },
+  {
+    business_id: "abcd",
+    name: "Beach Ventures Roofing",
+    state: "AZ",
+    city: "Phoenix",
+    stars: 3,
+    review_count: 30,
+  },
+  {
+    business_id: "abcd",
+    name: "Alpaul Automobile Wash",
+    city: "Charlotte",
+    state: "NC",
+    stars: 3,
+    review_count: 30,
+  },
+];
+
+describe("bestPlace", () => {
+  it("filters correctly", () => {
+    const list = new FluentBusinesses(testData).fromCityInState("Charlotte", "NC").getData();
+
+    assert(list.length === 3);
+    assert(list[0].name === "Applebee's");
+    assert(list[1].name === "China Garden");
+    assert(list[2].name === "Alpaul Automobile Wash");
+  });
+});
+
+describe("bestPlace", () => {
+  it("break tie with review count", () => {
+    const best = new FluentBusinesses(testData).fromCityInState("Charlotte", "NC").bestPlace();
+
+    assert(best);
+    assert(best.name === "China Garden");
+  });
+});
+
