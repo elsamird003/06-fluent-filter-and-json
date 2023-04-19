403c3466ba834e17b627dd8f15c7e6d89cb82526
Samuel Hernandez <71301115+elsamird003@users.noreply.github.com>
2023-04-18 22:43:56 -0400

    vv

--- src/FluentBusinesses.ts
+++ src/FluentBusinesses.ts
@@ -5,6 +5,7 @@ export class FluentBusinesses {
 
   constructor(data: Business[]) {
     this.data = data;
+  //  this.getData.arguments(this.data.hours);
   }
 
   getData(): Business[] {
@@ -45,11 +46,15 @@ export class FluentBusinesses {
       //const  ff   =  this.data(FluentBusinesses).filter(Business) => business.categories
   //return new FluentBusinesses(filteredBusinesses);
 }
-
-
   hasHoursOnDays(days: string[]): FluentBusinesses {
+      const filteredBusinesses = this.data.filter((business) => {
+        //Check if the business has hours on all specified days
+        return days.every((day) =>business.hours!.hasOwnProperty(day));
+      });
+  
+      // Return a new FluentBusinesses instance with the filtered businesses
+      return new FluentBusinesses(filteredBusinesses);
     // TODO
-    return this;
   }
 
   hasAmbience(ambience: string): FluentBusinesses {
@@ -58,19 +63,34 @@ export class FluentBusinesses {
   }
    
   // use the starts and review_count fields.
-  
-  hd(){
-    const ff =this.getData();
 
-    return ff;
-  }
 
   bestPlace(): Business | undefined {
 
-    // TODO
-   return undefined;
-
+    if (this.data.length === 0) {
+      return undefined;
     }
+
+    let bestBusiness: Business = this.data[0];
+
+    for (const business of this.data) {
+      if (
+        this.data.stars! > this.data.stars! ||
+        (business.stars === bestBusiness.stars &&
+          business.review_count > bestBusiness.review_count)
+      ) {
+        bestBusiness = business;
+      }
+    }
+
+    return bestBusiness;
+  }
+
+}
+    // TODO
+
+
+    
   mostReviews(): Business | undefined {
     // TODO
 
@@ -78,12 +98,11 @@ export class FluentBusinesses {
       //(Business) => Business.city === city && Business.state === state
     //);
     //return new FluentBusinesses(filteredBusinesses);
-      if(this.data.filter((Business) => Business.name === Business.attributes)){
-        return new this.mostReviews
+    //  if(this.data.filter((Business) => Business.name === Business.attributes)){
+       // return new this.mostReviews;
         
-      }
+      
     return undefined;
   }
 
-}
 
